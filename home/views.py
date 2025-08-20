4e800d1d46963d8afc20a5d2b8d944408642012c
NarendraSJ <narendrajadhavh1@gmail.com>
2025-08-21 00:32:29 +0530

    Bootstrap Navbar-1

--- home/views.py
+++ home/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render , HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, this is the home page ")
+    context = {
+        'message': 'Hello, this is the home page!'  ,
+        'message2': 'This is the second message',
+    }
+    # return HttpResponse("Hello, this is the home page ")
+    return render(request,'index.html')
 
-def about(request):
-    return HttpResponse("This is the about page ")
+def about(request): 
+    message="This is Super Star Narendra $$"
+    return HttpResponse("This is the about page "+message)
 
 def contact(request):
     return HttpResponse("This is the contact page ")
